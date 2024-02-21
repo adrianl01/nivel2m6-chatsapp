1c80c15ea635c01a97d9eaec185d53bde66a54fb
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2024-02-21 18:47:36 -0300

    24th commit endpoint link changed

--- endpoints.ts
+++ endpoints.ts
@@ -17,10 +17,6 @@ const createDocRoomsRef = doc(fsdb, "rooms/" + roomShortId.toString())
 
 // -----------------------------------------------
 
-app.get("/", (req, res) => {
-    res.status(200).send(console.log({ message: "Ok" }))
-})
-
 app.post("/signup", function (req, res) {
     const email = req.body.email; const name = req.body.name;
     console.log("datos en el sign up:" + email, name)
@@ -120,6 +116,9 @@ app.get("/rooms/:roomId", function (req, res) {
 app.use(express.static("dist"));
 app.get("*", () => { __dirname + "/dist/index.html" })
 
+app.get("/", (req, res) => {
+    res.status(200).send({ message: "Ok" })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at ${port}`)
